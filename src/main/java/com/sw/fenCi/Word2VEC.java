31c7a0e87b168ee331965b6eb0428355286ba9b8
songshipeng <songshipengchina@163.com>
2017-04-25 08:22:43 +0800

    路径修改

--- src/main/java/com/sw/fenCi/Word2VEC.java
+++ src/main/java/com/sw/fenCi/Word2VEC.java
@@ -39,9 +39,9 @@ public class Word2VEC {
 
     @PostConstruct
     public void afterPropertiesSet() throws IOException {
-//        loadModel("D:\\毕设\\ESpoem-vec.bin");
+        loadModel("D:\\毕设\\ESpoem-vec.bin");
 
-        loadModel("F:\\ownDev\\devData\\ESpoem-vec.bin");
+//        loadModel("F:\\ownDev\\devData\\ESpoem-vec.bin");
         System.out.println("One word analysis");
         Set<WordEntry> result = new TreeSet<WordEntry>();
         result = distance("思乡");//手动输入要计算的相关词
